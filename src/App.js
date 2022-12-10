631ccec1b1a26aab7f945dfc59a2578382b2389a
Gazalrafi <grafi7485>
2022-12-10 12:10:28 +0530

    Put the data in database

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { useSelector } from 'react-redux';
-
+import {useEffect} from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
+
+
+
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart=useSelector((state)=>state.cart);
 
+  useEffect(()=>{
+   fetch('https://expense-tracker-b6997-default-rtdb.firebaseio.com/newCart.json',{
+    method:'PUT',
+    body:JSON.stringify(cart),
+   }).then((response)=>{
+     alert ('data has succesfully sent')
+   })
+  },[cart]);
   return (
     <Layout>
       {showCart && <Cart />}
